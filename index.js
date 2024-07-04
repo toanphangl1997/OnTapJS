50e64296a57525a1f6da010fadb006e40ccb017c
ToanPhan <toanphangl1997@gmail.com>
2024-07-04 12:19:09 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// Ôn tập hiển thị nội dung lên giao diện
+import renderDuLieuGiay from "./util.js";
+import { duongDan as path } from "./util.js";
+// ôn tập hiển thị nội dung lên giao diện
+let duongDan = "Kiên nhóc";
 let arrMonAn = [
   {
     ten: "Mì xào",
@@ -31,28 +34,29 @@ for (let monAn of arrMonAn) {
   let { ten, soLuong, gia, trangThai } = monAn;
   if (trangThai) {
     content += `
-    <div class="col-3">
-                <!-- // chứa tên món -->
-                <h3>${ten}</h3>
-                <!-- chứa số lượng món  -->
-                 <p>${soLuong}</p>
-                 <!-- chứa giá tiền  -->
-                <p>${gia}</p>
-            </div>
-    `;
+   <div class="col-3">
+        <!-- chứa tên món  -->
+        <h3>${ten}</h3>
+        <!-- chứa số lượng món  -->
+        <p>${soLuong}</p>
+        <!-- chứa giá tiền  -->
+        <p>${gia}</p>
+      </div>
+  `;
   }
 }
 document.getElementById("baiTap1").innerHTML = content;
 
-// lấy dữ liệu từ backend
+// lấy dữ liệu từ BE
 function layDanhSachGiay() {
   let promise = axios({
     url: "https://shop.cyberlearn.vn/api/Product",
     method: "GET",
   });
+
   promise
     .then((res) => {
-      console.log(res.data.content);
+      console.log(res);
       // res.data.content
       renderDuLieuGiay(res.data.content);
     })
@@ -62,32 +66,11 @@ function layDanhSachGiay() {
 }
 layDanhSachGiay();
 
-// thực hiện render dữ liệu lên giao diện
-function renderDuLieuGiay(arrGiay, idTheCha = "baiTap2") {
-  let content = "";
-  for (let giay of arrGiay) {
-    let { image, name, shortDescription, price } = giay;
-
-    content += `
-        <div class="col-4">
-          <!-- hiển thị hình  -->
-          <img
-            src=${image}
-            class="w-100"
-            alt=""
-          />
-          <!-- tên sản phẩm  -->
-          <h4>${name}</h4>
-          <!-- mô tả ngắn -->
-          <p>
-            ${shortDescription}
-          </p>
-          <!-- số tiền  -->
-          <p>${price}</p>
-          <!-- nút chức năng mua ngay  -->
-          <button class="btn btn-dark">Mua ngay</button>
-        </div>
-        `;
-  }
-  document.getElementById(idTheCha).innerHTML = content;
+function duaNguoiDiToi(id) {
+  // window.location.href giúp chuyển hướng người dùng tới một trang web khác
+  // window.location.href = `https://cybersoft-crypto.vercel.app/detail.html?id=${id}`;
+  window.location.href = `http://127.0.0.1:5500/detail.html?id=${id}`;
 }
+
+console.log(duongDan);
+console.log(path);
